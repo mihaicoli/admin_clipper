415857504f39add1c97233a9bbbd13244a72ef23
Oliver Tonnhofer <olt@bogosoft.com>
2019-11-29 08:01:30 +0100

    setup: udpate metadata

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import pkg_resources
 
 setup(
     name='admin_clipper',
-    version='0.1.0',
+    version='0.1.1',
     description='Clip administrative boundaries at coastlines.',
     long_description='',
-    author='Oliver Tonnhofer',
-    author_email='olt@omniscale.de',
+    author='Omniscale',
+    author_email='info@omniscale.de',
     url='https://github.com/omniscale/admin_clipper',
     license='Apache Software License 2.0',
     py_modules=['admin_clipper'],
@@ -27,6 +27,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering :: GIS",
     ],
 )
